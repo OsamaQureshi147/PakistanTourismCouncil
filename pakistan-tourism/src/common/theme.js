44212ef9d4a0fff16dfe6b5b2362882ad19b3b52
Syed Akhter <dev.syed.akhter@gmail.com>
2022-12-24 22:13:43 +0500

    adds initial theme setup for app

---
+++ pakistan-tourism/src/common/theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    type: "light",
+    primary: {
+      main: "#009a42",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+    success: {
+      main: "#81f138",
+    },
+  },
+});
