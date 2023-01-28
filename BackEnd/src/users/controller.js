b5e07db30b8e87d7c4baed62ac4734dc8980a694
Osama Ehsan Qureshi <osamahsan@gmail.com>
2023-01-28 23:46:29 +0500

    adds sign up logic with hashed password

--- BackEnd/src/users/controller.js
+++ BackEnd/src/users/controller.js
@@ -1,6 +1,11 @@
-const { getAllUsersQuery, getSignUpUserQuery } = require("./queries");
+const {
+  getAllUsersQuery,
+  getSignUpUserQuery,
+  getUserByEmailQuery,
+} = require("./queries");
 
 const pool = require("../../databasepg");
+const { encryptPassword } = require("../../utils/helpers");
 
 const getAllUsers = (req, res) => {
   pool.query(getAllUsersQuery, (err, results) => {
@@ -9,12 +14,24 @@ const getAllUsers = (req, res) => {
   });
 };
 
-const signUpUser = (req, res) => {
+const signUpUser = async (req, res) => {
   const { name, email, password } = req.body;
-  pool.query(getSignUpUserQuery({ name, email, password }), (err, results) => {
-    if (err) res.status(500).send(`error occured while signing up:- ${err}`);
-    res.status(200).json(results);
-  });
+  const hashedPassword = await encryptPassword(password);
+  try {
+    const existingUsers = await pool.query(getUserByEmailQuery, [email]);
+    if (existingUsers.rows.length) {
+      return res.status(400).send("User already exists!");
+    } else {
+      const addUserResponse = await pool.query(getSignUpUserQuery, [
+        name,
+        email,
+        hashedPassword,
+      ]);
+      return res.status(200).send(addUserResponse.rows);
+    }
+  } catch (err) {
+    console.error("Error while adding user", err);
+  }
 };
 
 module.exports = { getAllUsers, signUpUser };
--- BackEnd/src/users/queries.js
+++ BackEnd/src/users/queries.js
@@ -1,10 +1,15 @@
 const getAllUsersQuery = "SELECT * FROM public.users";
 
-const getSignUpUserQuery = ({ name, email, password }) => {
-  return `INSERT INTO public.users (name, email, password) VALUES (${name}, ${email}, ${password}) RETURNING id;`;
+const getSignUpUserQuery =
+  "INSERT INTO public.users (name, email, password) VALUES ($1, $2, $3) RETURNING id";
+
+const getUserByEmailQuery = "SELECT * FROM public.users WHERE email = $1";
+
+// const Login =
+//   "Select * from public.users where email = 'd@g.com'and password = '123456'";
+
+module.exports = {
+  getAllUsersQuery,
+  getSignUpUserQuery,
+  getUserByEmailQuery,
 };
-
-const Login =
-  "Select * from public.users where email = 'd@g.com'and password = '123456'";
-
-module.exports = { getAllUsersQuery, getSignUpUserQuery, Login };
