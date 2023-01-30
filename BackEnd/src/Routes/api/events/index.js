bb5268cc10aefa3ed4bea7df559e9090de6a1c8e
Osama Ehsan Qureshi <osamahsan@gmail.com>
2023-01-31 01:53:34 +0500

    some code cleanup

--- BackEnd/src/Routes/api/events/index.js
+++ BackEnd/src/Routes/api/events/index.js
@@ -1,7 +1,7 @@
-const express = require("express");
+const { Router } = require("express");
 const eventController = require("../../../Controllers/events");
 
-const router = express.Router();
+const router = Router();
 
 router.get("/", eventController.getAllEvents);
 router.post("/", eventController.addNewEvent);
